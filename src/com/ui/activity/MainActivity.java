aeb90c6fe322206d40d767106e08d23e0d313ca0
Kemal Sami KARACA <kskaraca@gmail.com>
2015-02-09 22:58:36 +0200

    ** Market.001 ** First request done

--- src/com/ui/activity/MainActivity.java
+++ src/com/ui/activity/MainActivity.java
@@ -1,13 +1,27 @@
 package com.ui.activity;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import com.example.qr_market_android.R;
 import com.util.HttpHandler;
 
 import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,9 +30,12 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+	public String responseStr="xxx";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,21 +50,12 @@ public class MainActivity extends ActionBarActivity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+												
+				name.setText("Deneme");				
+														
 				
-				name.setText("DENEM");
+				new MyAsyncTask().execute("Deneme");							
 				
-				try {
-					String example = new HttpHandler().responseStr;
-					Log.i("HTTPHANDLER" , example);
-					
-					
-				} catch (ClientProtocolException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
 			}
 		});
 		
@@ -72,6 +80,69 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	
+	
+	private class MyAsyncTask extends AsyncTask<String, Integer, String>{
+		 
+		@Override
+		protected String doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			return postData(params[0]);
+			
+		}
+		
+		@Override
+	    protected void onPostExecute(String result) {
+			((EditText) findViewById(R.id.editText1)).setText(result);
+	    }
+ 
+		public String postData(String valueIWantToSend) {
+			String res="";
+			// Create a new HttpClient and Post Header
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost("http://10.0.2.2:8080/QR_Market_Web/Auth");
+ 
+			try {
+				// Add your data
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+				nameValuePairs.add(new BasicNameValuePair("myHttpData", valueIWantToSend));
+				httppost.setEntity((HttpEntity) new UrlEncodedFormEntity(nameValuePairs));				
+				
+				// Execute HTTP Post Request
+				HttpResponse response = httpclient.execute(httppost);
+				
+				
+				StatusLine statusLine = response.getStatusLine();
+			    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+			        ByteArrayOutputStream out = new ByteArrayOutputStream();
+			        response.getEntity().writeTo(out);
+			        res = out.toString();
+			        
+			        Log.i("RESPONSE" , responseStr);
+			        
+			        	
+			        
+			    } else{
+			        //Closes the connection.
+			        response.getEntity().getContent().close();
+			        throw new IOException(statusLine.getReasonPhrase());
+			    }
+				
+ 
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+			
+			return res;
+		}
+ 
+	}
+	
+	
 	
 	
 }
--- src/com/util/HttpHandler.java
+++ src/com/util/HttpHandler.java
@@ -2,37 +2,76 @@ package com.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-public class HttpHandler {
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class HttpHandler extends AsyncTask<String, Integer, Double> {
 	
-	public String responseStr;
+	public String responseStr = "Celal Selcuk";
 	
-	public HttpHandler() throws ClientProtocolException, IOException{
+	public HttpHandler(){
+					
 		
+	}
+
+	@Override
+	protected Double doInBackground(String... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 		
+	
+	public void postData(String valueIWantToSend) {
+		// Create a new HttpClient and Post Header
 		HttpClient httpclient = new DefaultHttpClient();
-	    HttpResponse response = httpclient.execute(new HttpGet("http://10.0.2.2:8080/QR_Market_Web/Auth"));
-	    StatusLine statusLine = response.getStatusLine();
-	    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-	        ByteArrayOutputStream out = new ByteArrayOutputStream();
-	        response.getEntity().writeTo(out);
-	        this.responseStr = out.toString();
-	        
-	    } else{
-	        //Closes the connection.
-	        response.getEntity().getContent().close();
-	        throw new IOException(statusLine.getReasonPhrase());
-	    }
-		
-		
+		HttpPost httppost = new HttpPost("http://10.0.2.2:8080/QR_Market_Web/Auth");
+
+		try {
+			// Add your data
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+			nameValuePairs.add(new BasicNameValuePair("myHttpData", valueIWantToSend));
+			httppost.setEntity((HttpEntity) new UrlEncodedFormEntity(nameValuePairs));				
+			
+			// Execute HTTP Post Request
+			HttpResponse response = httpclient.execute(httppost);
+									
+			StatusLine statusLine = response.getStatusLine();
+		    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+		        ByteArrayOutputStream out = new ByteArrayOutputStream();
+		        response.getEntity().writeTo(out);
+		        responseStr = out.toString();
+		        
+		        Log.i("RESPONSE" , responseStr);
+		        
+		    } else{
+		        //Closes the connection.
+		        response.getEntity().getContent().close();
+		        throw new IOException(statusLine.getReasonPhrase());
+		    }
+			
+
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		}
 	}
 
 }
